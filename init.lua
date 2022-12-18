59f9954f122afbc8a7280ec1ffeef2ac50477128
Lew1s777 <lew1s0620@outlook.com>
2022-12-18 17:34:53 +0800

    test

--- init.lua
+++ init.lua
@@ -5,12 +5,14 @@
 --| |\  |  __/ (_) \ V / | | | | | | |  _ <| |___ 
 --|_| \_|\___|\___/ \_/  |_|_| |_| |_|_| \_\\____|
 --                                                
---If you find that vim suddenly becomes very jammed, try running the following command in ur shell.
---rm ~/.local/share/nvim/shada/main.shada
 --
--- install the plugin manager [packer](https://github.com/wbthomason/packer.nvim) with the command below or with the aur package "aur/nvim-packer-git" if u r using arch linux
+-- install plugin manager [packer](https://github.com/wbthomason/packer.nvim) with the command below
 -- git clone --depth 1 https://github.com/wbthomason/packer.nvim\
 --  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+
+--rm ~/.local/share/nvim/shada/main.shada
+
 
 -- manageing plugins[./lua/plugins.lua]
 require('plugins')
